91e344003120efd39fecfb7279ecd3e748aeea4e
Serafim Nenarokov <serafim.nenarokov@flant.com>
2017-01-24 12:59:20 +0100

    Fixes bug with LastModelUpdate#last_message_at initializing

--- app/models/artery/last_model_update.rb
+++ app/models/artery/last_model_update.rb
@@ -4,7 +4,7 @@ if defined?(ActiveRecord)
       class << self
         def model_update!(uri, timestamp)
           obj = find_or_initialize_by service: uri.service, model: uri.model
-          obj.last_message_at = timestamp if timestamp.to_f > obj.last_message_at.to_f
+          obj.last_message_at = Time.zone.at(timestamp) if timestamp.to_f > obj.last_message_at.to_f
           obj.save!
         end
 
