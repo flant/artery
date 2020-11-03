21d81941774367ce816c401a4b6aca9137a0dc66
Sergey Gnuskov <sergey.gnuskov@flant.com>
2020-11-03 16:25:16 +0300

    Fix ORM models autoloading

---
+++ lib/artery/active_record.rb
@@ -0,0 +1,6 @@
+module Artery
+  module ActiveRecord
+    autoload :Message,          'artery/active_record/message'
+    autoload :SubscriptionInfo, 'artery/active_record/subscription_info'
+  end
+end
---
+++ lib/artery/no_brainer.rb
@@ -0,0 +1,6 @@
+module Artery
+  module NoBrainer
+    autoload :Message,          'artery/no_brainer/message'
+    autoload :SubscriptionInfo, 'artery/no_brainer/subscription_info'
+  end
+end
