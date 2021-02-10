f374f2d944741429f5ca3ccfb93b87ec647f9996
Sergey Bondarenko <45511623+sb-truefalse@users.noreply.github.com>
2021-02-10 16:56:14 +0300

    [+] Fake backend (#33)

---
+++ lib/artery/backends/fake.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# For the test environment
+module Artery
+  module Backends
+    class Fake < Base
+
+      def start(*_args); end
+
+      def connect(*_args); end
+
+      def subscribe(*_args); end
+
+      def unsubscribe(*_args); end
+
+      def publish(*_args); end
+
+      def request(route, data, opts = {})
+        yield if block_given?
+      end
+
+      def stop(*_args); end
+    end
+  end
+end
