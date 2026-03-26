7b87a250926071780ba3993487a046e908065224
Sergey Gnuskov <sergey.gnuskov@flant.com>
2026-03-26 11:44:28 +0100

    Non-atomic notifications support (#84)
    This should help to handle very-loaded models and reduce lock wait time

--- lib/artery/model/callbacks.rb
+++ lib/artery/model/callbacks.rb
@@ -15,7 +15,11 @@ module Artery
           after_unarchive :artery_on_unarchive
         end
 
-        before_commit :artery_send_pending_notifications
+        if artery[:non_atomic_notification]
+          after_commit :artery_send_pending_notifications
+        else
+          before_commit :artery_send_pending_notifications
+        end
       end
 
       def artery_on_create
@@ -45,11 +49,22 @@ module Artery
       end
 
       def artery_send_pending_notifications
-        artery_pending_notifications.each do |action, extra_data|
-          artery_notify_message(action, extra_data || {})
+        attempts = 0
+        begin
+          artery_pending_notifications.each do |action, extra_data|
+            artery_notify_message(action, extra_data || {})
+          end
+        rescue StandardError => e
+          attempts += 1
+          retry if self.class.artery[:non_atomic_notification] && attempts <= 3
+
+          Artery.handle_error Artery::Error.new(
+            "Failed to send artery notifications after #{attempts} attempts: #{e.message}",
+            original_exception: e
+          )
+        ensure
+          @artery_pending_notifications = nil
         end
-      ensure
-        @artery_pending_notifications = nil
       end
     end
   end
