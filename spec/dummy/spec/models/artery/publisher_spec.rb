b16d3e8d212f2af927562e138ae2b99b27d7ef8c
Sergey Gnuskov <sergey.gnuskov@flant.com>
2026-03-30 10:45:25 +0200

    Fix models not processed if models count > threads count

--- spec/dummy/spec/models/artery/publisher_spec.rb
+++ spec/dummy/spec/models/artery/publisher_spec.rb
@@ -3,16 +3,16 @@
 RSpec.describe Artery::Publisher do
   subject(:publisher) { described_class.new }
 
-  describe '#publish_batch (via send)' do
-    let(:model_name) { 'source' }
+  let(:model_name) { 'source' }
 
-    before do
-      Artery::ActiveRecord::ModelInfo.find_or_create_by!(model: model_name) do |r|
-        r.latest_index = 0
-        r.last_published_id = 0
-      end
+  before do
+    Artery::ActiveRecord::ModelInfo.find_or_create_by!(model: model_name) do |r|
+      r.latest_index = 0
+      r.last_published_id = 0
     end
+  end
 
+  describe '#publish_batch (via send)' do
     it 'returns 0 when no unpublished messages exist' do
       result = publisher.send(:publish_batch, model_name)
       expect(result).to eq(0)
@@ -37,7 +37,7 @@ RSpec.describe Artery::Publisher do
       expect(row.last_published_id).to eq(messages.last.id)
     end
 
-    it 'builds correct _previous_index chain' do
+    it 'builds correct _previous_index chain' do # rubocop:disable RSpec/MultipleExpectations
       3.times { create(:source) }
       messages = Artery.message_class.where(model: model_name).order(:id).to_a
 
@@ -70,6 +70,35 @@ RSpec.describe Artery::Publisher do
 
       expect(received.size).to eq(1)
       expect(received[0]['_previous_index']).to eq(first_message.id)
+    end
+  end
+
+  describe '#process_model (via send)' do
+    it 'publishes all pending messages and releases' do
+      3.times { create(:source) }
+
+      received = []
+      Artery.subscribe('test.source.create') { |m| received << m }
+
+      publisher.send(:process_model, model_name)
+
+      sleep 0.2
+
+      expect(received.size).to eq(3)
+
+      row = Artery::ActiveRecord::ModelInfo.find_by!(model: model_name)
+      expect(row.last_published_id).to eq(Artery.message_class.where(model: model_name).maximum(:id))
+    end
+
+    it 'handles errors without raising' do
+      allow(Artery.model_info_class).to receive(:transaction).and_raise(StandardError, 'db gone')
+      allow(Artery).to receive(:handle_error)
+
+      expect { publisher.send(:process_model, model_name) }.not_to raise_error
+
+      expect(Artery).to have_received(:handle_error).with(
+        an_instance_of(Artery::Error).and(having_attributes(message: /db gone/))
+      )
     end
   end
 end
